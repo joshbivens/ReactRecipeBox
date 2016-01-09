fe903269e1b9af4ad90e434d7636c3c6f1a038c9
iheartkode <markhjr@outlook.com>
2016-01-08 17:17:39 -0800

    adding recipes component

--- src/components/RecipeForm.jsx
+++ src/components/RecipeForm.jsx
@@ -5,29 +5,29 @@ export default class extends React.Component {
     return (
       <div>
         <form>
-      
+
           <div>
             <input type="text" id="name" placeholder="Recipe Name" min="0"/>
           </div>
-        
+
           <div>
             <input type="number" id="prep" name="prep" placeholder="Prep Time in minutes" min="0"/>
           </div>
-        
+
           <div>
             <input type="number" id="cook" name="cook" placeholder="Cook Time"/>
           </div>
 
           <div>
-            <textarea id="ingredients" class="materialize-textarea" placeholder="Ingredients"></textarea>
+            <textarea id="ingredients" className="materialize-textarea" placeholder="Ingredients"></textarea>
           </div>
 
           <div>
-            <textarea id="steps" class="materialize-textarea" placeholder="Steps"></textarea>
+            <textarea id="steps" className="materialize-textarea" placeholder="Steps"></textarea>
           </div>
-        
+
           <input id="add" type="submit" value="Add"/>
-        
+
         </form>
       </div>
     )
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RecipeForm from './components/RecipeForm';
+import Recipes from './components/Recipes';
 
 export class App extends React.Component {
 	render() {
 		return (
       <div>
         <RecipeForm />
+				<Recipes />
       </div>
 		);
 	}
